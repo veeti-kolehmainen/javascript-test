48c87f03c0e1ee68d9a8f3a9271120c8d240072f
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-03-09 12:17:20 +0200

    made a few variables and an alert

---
+++ scripts/javascript.js
@@ -0,0 +1,7 @@
+var myname = "Veeti";
+var course = "JavaScript for Beginners";
+var myhobby = "gaming";
+var age = 16;
+alert("My name is " + myname + " and my age is " + age + ". I am currently doing " + course + ". My hobby is " + myhobby + ".");
+//console.log(age);
+//var introduction = "Your name is " + age + " and your age is " + myname;
