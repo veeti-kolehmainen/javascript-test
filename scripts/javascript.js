b2fd55f66140420764cb05c95f73b5be3e478a67
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-03-09 13:43:33 +0200

    a

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,7 +1,12 @@
-var myname = "Veeti";
+var myName = "Veeti";
 var course = "JavaScript for Beginners";
-var myhobby = "gaming";
+var myHobby = "gaming";
 var age = 16;
-alert("My name is " + myname + " and my age is " + age + ". I am currently doing " + course + ". My hobby is " + myhobby + ".");
-//console.log(age);
+var intro = "My name is " + myName + " and my age is " + age + ". I am currently doing " + course + ". My hobby is " + myHobby + "."
+//alert(intro);
 //var introduction = "Your name is " + age + " and your age is " + myname;
+console.log(intro);
+
+var test = document.getElementById("test");
+var oldText = test.innerHTML;
+test.innerHTML = "Hi welcome to JavaScript for beginners";
