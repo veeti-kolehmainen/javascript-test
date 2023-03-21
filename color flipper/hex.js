e17db1082c8983367746ff351832735734db5061
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-03-21 09:25:20 +0200

    made cool stuff

--- color flipper/hex.js
+++ color flipper/hex.js
@@ -1,8 +1,10 @@
+// possible hex color
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-// #f15025
+
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
+// button that chooses a random number between 0-15 and repeats it 6 times
 btn.addEventListener("click", function(){
     let hexColor = "#";
     for(let i = 0;i<6;i++){
@@ -11,7 +13,7 @@ btn.addEventListener("click", function(){
     color.textContent = hexColor;
     document.body.style.backgroundColor = hexColor;
 });
-
+// random number generator 0-15
 function getRandomNumber(){
     return Math.floor(Math.random() * hex.length);
 }
