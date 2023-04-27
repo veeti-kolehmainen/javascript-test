ae82c38e9089daf40fb8a5c29941d83487e9fb36
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-04-27 09:16:20 +0300

    removed the browser crashing

--- guessing_game/script.js
+++ guessing_game/script.js
@@ -11,9 +11,10 @@ while(number != 23) {
 guessStatus.innerText = "Status: GUESSED!";
 console.log("CORRECT!");
 alert("YOU WON!! CONGRATULATIONS!!!");
-while(1 < 2) {
-    console.log("GET CRASHED!")
-}
 
 document.body.style.backgroundImage = "url('explosion.png')";
 document.body.style.color = "white";
+
+/*while(1 < 2) {
+    console.log("GET CRASHED!")
+}*/
