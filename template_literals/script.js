66ff4d5f4dcf80388aa645e79e86870906213f83
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-05-03 08:35:39 +0300

    experimented with template literals

---
+++ template_literals/script.js
@@ -0,0 +1,5 @@
+var userName = "kalob";
+//console.log("Hi my name is " + userName + " more stuff here " + userName);
+console.log(`Hi my name is ${userName}`)
+
+const sentence = `Hello, my name is ${userName}, welcome to my course on JavaScript`
