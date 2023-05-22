f4420d1bb7d9e27f5d3addb608b926fc15883ada
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-05-22 08:39:28 +0300

    tried some stuff

---
+++ arrow_functions/script.js
@@ -0,0 +1,45 @@
+// (function somename(one, two) {
+//     return athing
+// })();
+// const kalob = function() {
+//     ....
+//     return "kalob"
+// }
+
+// function something() {
+//     this
+// }
+// class NewClass {
+//     funcname() {
+//         this
+//     }
+// }
+// const person = {
+//     funcname: function() {
+//         this
+//     }
+// }
+
+const arr = ["JavaScript", "Python", "PHP"]
+
+arr.forEach(language => {
+    console.log(language)
+});
+
+setTimeout(() => {
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+    console.log("YAY I WAITED")
+}, 3000)
+
+
+// greet = (name, place) => `Hello ${name} ${place}`
+
+
+// const hi = greet("Kalob", "Canada")
+// console.log(hi)
