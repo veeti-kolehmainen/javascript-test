96c17b274130d343e9f9a3396ed2ac3645c3f0a0
veeti-kolehmainen <112861451+veeti-kolehmainen@users.noreply.github.com>
2023-08-09 11:07:56 +0300

    finished the memory game

--- memory game/script.js
+++ memory game/script.js
@@ -3,6 +3,18 @@ const numberOfSquares = 16
 let i = 0
 let square1, square2;
 let clickCount = 0;
+let score = 0;
+let ding = new Audio("ding.wav")
+document.querySelector("embed").style.visibility = "hidden"
+const playAgainBtn = document.querySelector("button")
+playAgainBtn.style.visibility = "hidden"
+playAgainBtn.addEventListener("click", playAgain)
+
+
+// bgMusic.play();
+// bgMusic.loop = true;
+
+document.querySelector("#score").style.visibility = "hidden";
 
 let colors = [
     "#33ff33",
@@ -47,13 +59,67 @@ for(const square of squares) {
 }
 
 function squareClicked() {
+    if(square1 == this) return;
     clickCount++;
+    if(clickCount > 2) return;
     clickCount === 1 ? (square1 = this) : (square2 = this);
     if(clickCount === 1) {
         square1.style.background = square1.getAttribute("data-color")
     } else {
         square2.style.background = square2.getAttribute("data-color")
+        checkMatch()
     }
+}
+
+function checkMatch() {
+    let match = square1.getAttribute("data-color") === square2.getAttribute("data-color")
+    if(!match) {
+        square1.classList.add("shake");
+        square2.classList.add("shake");
+        setTimeout(function() {
+            noMatch()
+        }, 500)
+    } else {
+        isMatch()
+        checkGameEnded()
+    }
+}
+
+function noMatch() {
+    square1.style.background = ""
+    square2.style.background = ""
+    square1.classList.remove("shake");
+    square2.classList.remove("shake");
+    square1 = "";
+    square2 = "";
+    clickCount = 0;
+    console.log("no match")
+}
+function isMatch() {
+    score++;
+    ding.play()
+    document.querySelector("#score").innerText = score;
+    document.querySelector("#score").style.visibility = "visible";
+    square1.classList.add("pop");
+    square2.classList.add("pop");
+    square1.style.border = "none"
+    square2.style.border = "none"
+    square1.removeEventListener("click", squareClicked);
+    square2.removeEventListener("click", squareClicked);
+    clickCount = 0;
+    console.log("is a match")
+}
+
+function checkGameEnded() {
+    const target = numberOfSquares / 2;
+    const gameOver = score === target ? true : false
+    if(gameOver) {
+        playAgainBtn.style.visibility = "visible"
+    }
+}
+
+function playAgain() {
+    window.location.reload();
 }
 // selectColor()
 // selectColor()
